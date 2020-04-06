0ff3d66e7974aa5acc7414ded21b6a675d7de07a
AndreaArdu <44276399+AndreaArdu@users.noreply.github.com>
2020-04-07 00:05:58 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,8 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems.",
+				"learn net cool things.", "climb mountains.", "enjoy life."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
